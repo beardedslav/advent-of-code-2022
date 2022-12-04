83b11394d6149a2ff33986dd464a0e19323e339c
Krzysztof Malinowski <kmalinowski@Belial.broadband>
2022-12-04 20:40:57 +0000

    Day 4 solution - flipped to correct scripts

--- src/day4/day4part2.py
+++ src/day4/day4part2.py
@@ -7,10 +7,9 @@ def main(args):
     with open('data/day4/{}'.format(args.inputFileName)) as inputFile:
         inputFileContents = inputFile.read()
     st = time.process_time()
-    contains = lambda pair1, pair2 : int((pair1[0]<= pair2[0] and pair1[1] >= pair2[1]) or (pair1[0]>= pair2[0] and pair1[1] <= pair2[1]))
-    print(sum(map( lambda pair : contains(list(map(int,(pair[0].split('-')))), list(map(int,(pair[1].split('-'))))), map(lambda l: l.split(','),inputFileContents.split('\n')))))
-
-    
+    numsBetween = lambda a , b ,c: a <= b and b <= c
+    overlaps = lambda pair1, pair2 : int(numsBetween(pair1[0], pair2[0], pair1[1]) or numsBetween(pair2[0], pair1[0], pair2[1]))
+    print(sum(map( lambda pair : overlaps(list(map(int,(pair[0].split('-')))), list(map(int,(pair[1].split('-'))))), map(lambda l: l.split(','),inputFileContents.split('\n')))))
     et = time.process_time()
     print('Calc time: {:.6f}s'.format(et-st))
     
